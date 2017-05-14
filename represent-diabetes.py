bd7c07cdb66ae51b07afbed6bf86c70dd36c7034
Rana-ElRobi <eng.rana.abdel-haleem@ieee.org>
2017-05-14 19:44:59 +0200

    visualised all the dimansions

--- represent-diabetes.py
+++ represent-diabetes.py
@@ -39,17 +39,40 @@ def main():
 	not_sickAge=[]
 	sickPreg =[]
 	not_sickPreg=[]
+	sickSkin=[]
+	not_sickSkin = []
+	sickGlo=[]
+	not_sickGlo=[]
 	for s in range (0,len(age)):
 		if (label[s] ==0):
 			not_sickAge.append(age[s])
 			not_sickPreg.append(pregrnancies[s])
+			not_sickSkin.append(skinThickness[s])
+			not_sickGlo.append(glucose[s])
 		else:
 			sickAge.append(age[s])
 			sickPreg.append(pregrnancies[s])
-	# plot data as sick and not sick 
-	plt.figure(figsize=(10, 6), dpi=80)
-	plt.plot(not_sickAge, not_sickPreg, 'bo', color="blue")
-	plt.plot(sickAge, sickPreg,'bo' ,color="red")
+			sickSkin.append(skinThickness[s])
+			sickGlo.append(glucose[s])
+	# lets now make another sub groups with the size 
+	# of the point as size of glocose or skinthikness
+	plt.figure(figsize=(74, 50), dpi=70)
+	for i in range (0, len(not_sickSkin)):
+		plt.plot(not_sickAge[i], not_sickPreg[i], 'bo', markersize=not_sickSkin[i], markeredgecolor='black', color="blue")
+	for j in range (0, len(sickSkin)):
+		plt.plot(sickAge[j], sickPreg[j],'bo' ,markersize=sickSkin[j], markeredgecolor='black',color="red")
+	# ------------------
+	plt.xlabel('Women Age')
+	plt.ylabel('Number of pregrnancies')
+	plt.title('Age vs #pregrnancies vs Sick or not vs Skinthikness')
+	plt.grid(True)
+	plt.xlim((15,90))
+	plt.ylim((-4,20))
+	#def onclick(event):
+    #print('button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
+    #      (event.button, event.x, event.y, event.xdata, event.ydata))
+	#cid = fig.canvas.mpl_connect('button_press_event', onclick)
+	#plt.canvas.mpl_disconnect(cid)
 
 	# Save figure using 72 dots per inch
 	# plt.savefig("exercice_2.png", dpi=72)
